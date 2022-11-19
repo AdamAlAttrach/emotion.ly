76448ac7e78a4eeeeddcf96eb33457e6f0541a02
Diego <diego007.castillo@gmail.com>
2022-11-19 16:20:08 -0500

    App.js revert

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,17 @@
-import React, {useEffect, useState} from 'react'
-import _axios from 'axios';
-
-
-const axios = _axios.create({ baseURL: 'http://localhost:5000'});
+import React from 'react'
+import InputText from './components/InputText'
+import Layout from "./components/InputText"
+import './index.css'
+import Genres from './components/genres/Genres'
 
 function App() {
-  
-  const params = {"text": "I am got an internship finally, I'm so happy, so relieved wow finally!",
-                  "genres": "hip-hop"
-                }
-  const [songs, setSongs] = useState([])
-  
-  useEffect( () => {
-    getSongs();
-  }, []);
-
-  const getSongs = () => {
-    axios.get('/songs', {params}).then((response) => {
-      console.log(response.data)
-      const allSongs = response.data;
-      setSongs(allSongs.tracks)
-    })
-    .catch(() => console.log("error"))
-  }
-
   return (
     <div>
-      {songs.map( (song) => <h1>{song.name}</h1> )}
+      <InputText />
+      <Genres />
+
     </div>
+
   )
 }
 
