eb088fc2a9a4061513a27fdbb9196ab48152519b
Sam-Mobed <masdebom@gmail.com>
2022-11-19 13:50:45 -0500

    genres folder
    Worked on the genres menu, creating a folder for it that contains a genres.css and genres.js and a folder that contains the images we'll use

---
+++ client/src/components/genres/genres.js
@@ -0,0 +1,176 @@
+import './genres.css';
+
+const genres = () => {
+    return (
+        <nav className="genres-menu">
+            <div className="genre Pop" >
+                <a>Pop</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Pop</span>
+                        <span>Pop</span>
+                        <span>Pop</span>
+                        <span>Pop</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Rock">
+                <a>Rock</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Rock</span>
+                        <span>Rock</span>
+                        <span>Rock</span>
+                        <span>Rock</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Jazz">
+                <a>Jazz</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Jazz</span>
+                        <span>Jazz</span>
+                        <span>Jazz</span>
+                        <span>Jazz</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Metal">
+                <a>Metal</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Metal</span>
+                        <span>Metal</span>
+                        <span>Metal</span>
+                        <span>Metal</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Classical">
+                <a>Classical</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Classical</span>
+                        <span>Classical</span>
+                        <span>Classical</span>
+                        <span>Classical</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Hip-Hop">
+                <a>Hip-Hop</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Hip-Hop</span>
+                        <span>Hip-Hop</span>
+                        <span>Hip-Hop</span>
+                        <span>Hip-Hop</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Electronic">
+                <a>Electronic</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Electronic</span>
+                        <span>Electronic</span>
+                        <span>Electronic</span>
+                        <span>Electronic</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Alternative">
+                <a>Alternative</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Alternative</span>
+                        <span>Alternative</span>
+                        <span>Alternative</span>
+                        <span>Alternative</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Dubstep">
+                <a>Dubstep</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Dubstep</span>
+                        <span>Dubstep</span>
+                        <span>Dubstep</span>
+                        <span>Dubstep</span>
+                    </div>
+                </div>  
+            </div>
+            <div className="genre Latin">
+                <a>Latin</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Latin</span>
+                        <span>Latin</span>
+                        <span>Latin</span>
+                        <span>Latin</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Soul">
+                <a>Soul</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Soul</span>
+                        <span>Soul</span>
+                        <span>Soul</span>
+                        <span>Soul</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Blues">
+                <a>Blues</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Blues</span>
+                        <span>Blues</span>
+                        <span>Blues</span>
+                        <span>Blues</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre R-n-B">
+                <a>R-n-B</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>R-n-B</span>
+                        <span>R-n-B</span>
+                        <span>R-n-B</span>
+                        <span>R-n-B</span>
+                    </div>
+                </div>
+            </div>
+            <div className="genre Reggae">
+                <a>Reggae</a>
+                <img></img>
+                <div className="effect">
+                    <div>
+                        <span>Reggae</span>
+                        <span>Reggae</span>
+                        <span>Reggae</span>
+                        <span>Reggae</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
