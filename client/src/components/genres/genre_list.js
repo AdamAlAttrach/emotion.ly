2692e1b9675ce023c2c100d6714f3886bf6228d2
AdamAlAttrach <adamalattrach@gmail.com>
2022-11-20 05:11:47 -0500

    yea

--- client/src/components/genres/genre_list.js
+++ client/src/components/genres/genre_list.js
@@ -28,67 +28,62 @@ function GenresList(){
         {
           title: "Jazz",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/jazz.jpg"
         },
         {
           title: "Metal",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/metal.jpg"
         },
         {
           title: "Classical",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/classical.jpg"
         },
         {
           title: "Hip-Hop",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/hiphop.jpg"
         },
         {
           title: "Electronic",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/electronic.jpg"
         },
         {
           title: "Country",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/country.jpg"
         },
         {
           title: "Latin",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/latin.jpg"
         },
         {
           title: "Dubstep",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/dubstep.jpg"
         },
         {
           title: "Soul",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/soul.jpg"
         },
         {
           title: "Blues",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/blues.jpg"
         },
         {
           title: "RnB",
           isSelected: false,
-          image: "./images/pop.jpg"
-        },
-        {
-          title: "Rock",
-          isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/rnb.jpg"
         },
         {
           title: "Reggae",
           isSelected: false,
-          image: "./images/pop.jpg"
+          image: "./images/reggae.jpg"
         },
         
       ]
