8b964f36e0828b1db165c2ebe00d91e65ab87ea6
Ser1212 <sergionajm@gmail.com>
2022-11-19 19:23:07 -0500

    more

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,23 +3,26 @@ from flask_cors import CORS
 from helpers import helper_functions as hf
 
 
-
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route('/')
 def home():
     return "<p>Hello, World!</p>"
 
 # Text analysis route
+
+
 @app.route("/playlist")
 def getSongs():
     genres = request.args.get("genres")
     text = request.args.get("text")
     emotion = hf.get_emotion_keywords(text)
     valence, energy = hf.get_valence_energy(emotion)
-    return hf.get_songs(valence, energy, 10, genres)
-    
+    songs = hf.get_songs(valence, energy, 10, genres)
+    return hf.add_mp3_url(songs)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
